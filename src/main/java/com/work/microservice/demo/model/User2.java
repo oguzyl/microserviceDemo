cc5b88052fe44f7d1bebd2aa87265c4b7c9905d4
oguzyildirim <yildirimogz@gmail.com>
2023-02-09 15:32:48 +0300

    denemeler

--- src/main/java/com/work/microservice/demo/model/User2.java
+++ src/main/java/com/work/microservice/demo/model/User2.java
@@ -1,18 +1,19 @@
 package com.work.microservice.demo.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 @Entity(name = "user_details")
-public class User {
+public class User2 {
 
     @Id
-    @SequenceGenerator(name = "userIdSequence", sequenceName = "user_details_seq", allocationSize = 1)
-    @GeneratedValue(generator = "userIdSequence", strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
     private Integer id;
 
 //    @JsonProperty("user_name")
@@ -21,14 +22,14 @@ public class User {
 //    @JsonProperty("birth_dates")
     private LocalDate birthDate;
 
-    public User(Integer id, String name, LocalDate birthDate) {
+    public User2(Integer id, String name, LocalDate birthDate) {
         super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
     }
 
-    public User() {
+    public User2() {
 
     }
 }
